7d315dee136bc75b146e25182e71e7157cad0d74
Noah Holt <70286103+TidalTurtles@users.noreply.github.com>
2021-02-20 15:09:55 -0700

    Add files via upload

---
+++ HoltNoahAssignment4.java
@@ -0,0 +1,20 @@
+
+public class HoltNoahAssignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				/*Noah Holt
+				 * CS1150 Principles of computer science
+				 * m/w section
+				 * Due 24 feb
+				 * Assignment 4
+				 * This assignment is to create a license plate generator for a three state area
+				 *  based off which state the user chooses and which type of license plate they want
+				 */
+		
+		
+		
+	}
+
+}
